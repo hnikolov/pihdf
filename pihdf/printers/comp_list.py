3a852a105ba9cb85c789167f175888fbaa5d6be9
hristo <h.n.nikolov@gmail.com>
2016-03-24 22:16:27 +0100

    The pragmas used to facilitate command 'update' are removed from compile_list.txt file. Now, the assumption is that all custom verilog files are located within the module's source directory (can be in sub-directories).

--- pihdf/printers/comp_list.py
+++ pihdf/printers/comp_list.py
@@ -1,25 +1,49 @@
 from str_builder import StrBuilder
 import os
 
+from .. import mylog
+
 def print_compile_list_file(mfdo):
     '''|
     | Create 'compile_list.txt' file containing a default list of verilog files used in case of co-simulation
     | All verilog files, which are part of the MyHDL design but are not auto-generated must be specified in this 'compile_list.txt' file
     |________'''
     s = StrBuilder()
-    filename =  mfdo.c_path + '/' + mfdo.src_path + '/compile_list.txt'
+    filename = mfdo.c_path + '/' + mfdo.src_path + '/compile_list.txt'
+    filepath = os.getcwd() + '/' + mfdo.c_path + '/' + mfdo.src_path
 
     s += mfdo.module_name + '_top.v\n'
+
     if mfdo.verilog != {}:
-        if mfdo.verilog["path"]=='':
-            s += os.getcwd() + '/' + mfdo.c_path + '/' + mfdo.src_path + '/' + mfdo.module_name + '.v\n'
+        ls = extractFileNames(filename, mfdo)
+
+        if ls == []:
+            if mfdo.verilog["path"] == '':
+                s += filepath + '/' + mfdo.module_name + '.v\n'
+            else:
+                s += filepath + '/' + mfdo.verilog["name"] + '.v\n'
         else:
-            s += os.getcwd() + '/' + mfdo.c_path + '/' + mfdo.src_path + '/' + mfdo.verilog["name"] + '.v\n'
-            
-    s += '#--- Add files begin ---#\n'
-    # TODO: We should somehow update the relative path to the verilog files. Currently, we do nothing."
-    s += s.noIndent() + mfdo.extractText(filename, "#--- Add files begin ---#", "#--- Add files end   ---#")
-    s += "#--- Add files end   ---#\n"
-            
+            for l in ls:
+                s += filepath + l + '\n'
+
     s += 'tb_' + mfdo.module_name + '_top.v\n'
     s.write(filename, overwrite = mfdo.overwrite)
+
+
+def extractFileNames(filename, mfdo):
+    '''|
+    | Extract the names of the 'additional' custom verilog files used to implement the verilog design
+    | Assumes that the files are located inside the module's source directory
+    |________'''
+    ls = []
+    try:
+        with open(filename) as f:
+            for line in f:
+                if mfdo.src_path in line:
+                    ls.append( line.partition(mfdo.src_path)[2].strip() )
+
+    except Exception as e:
+        mylog.err("in extractFileNames():")
+        print "      ", e
+
+    return ls
