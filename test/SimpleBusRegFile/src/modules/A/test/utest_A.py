38982638ed21b2a66cced3de33839bf4288bbc4a
Hristo Nikolov <h.n.nikolov@gmail.com>
2017-03-26 19:55:11 +0200

    Mmodule A -> Test updated.

--- test/SimpleBusRegFile/src/modules/A/test/utest_A.py
+++ test/SimpleBusRegFile/src/modules/A/test/utest_A.py
@@ -33,43 +33,30 @@ class Test_A(t_A):
         fields_in = { 'addr': 0, 'data': 11 }
         self.stim_sbus_wa_wd.append( fields_in )
 
-        fields_in = { 'addr': 0, 'data': 22 }
+        fields_in = { 'addr': 1, 'data': 22 }
         self.stim_sbus_wa_wd.append( fields_in )
 
-        #--- big_register --
-#        fields_in = { 'addr': 3, 'data':1}
-#        self.stim_sbus_wa_wd.append( fields_in )
-
-        fields_in = { 'addr': 4, 'data': 2 }
+        fields_in = { 'addr': 2, 'data': 2 }
         self.stim_sbus_wa_wd.append( fields_in )
-
-        fields_in = { 'addr': 5, 'data': 3 }
-        self.stim_sbus_wa_wd.append( fields_in )
-        #---
 
         fields_in = { 'addr': 13, 'data': 33 }  # WRONG ADDRESS
         self.stim_sbus_wa_wd.append( fields_in )
 
-        self.cond_sbus_wa_wd += [(1,("sbus_raddr", 0)),
-                                 (2,("sbus_raddr", 1))]
+#        self.cond_sbus_wa_wd += [(1,("sbus_raddr", 0)),
+#                                 (2,("sbus_raddr", 1)),
+#                                 (3,("sbus_raddr", 2)),
+#                                 (4,("sbus_raddr", 3))]
 
-        self.cond_sbus_raddr += [(0,("sbus_wa_wd", 0)),
-                                 (1,("sbus_wa_wd", 1)),
-                                 (2,("sbus_wa_wd", 4)),
-                                 (5,("sbus_wa_wd", 5))]
+        self.cond_sbus_raddr += [(0,("sbus_wa_wd", 3))]
 
         self.stim_sbus_raddr.append({"data": 0})
-        self.stim_sbus_raddr.append({"data": 0})
-#        self.stim_sbus_raddr.append({"data": 3})
-        self.stim_sbus_raddr.append({"data": 4})
-        self.stim_sbus_raddr.append({"data": 5})
+        self.stim_sbus_raddr.append({"data": 1})
+        self.stim_sbus_raddr.append({"data": 2})
         self.stim_sbus_raddr.append({"data": 13}) # WRONG ADDRESS: No response - no ref data assigned
 
         self.ref_sbus_rdata.append({"data": 11})
-        self.ref_sbus_rdata.append({"data": 22})
-#        self.ref_sbus_rdata.append({"data": 1})
+        self.ref_sbus_rdata.append({"data": 0})  # Global register is declared as write only
         self.ref_sbus_rdata.append({"data": 2})
-        self.ref_sbus_rdata.append({"data": 3})
         self.ref_sbus_rdata.append({"data": 0xFFFFFFFF})
 
         self.run_it()
