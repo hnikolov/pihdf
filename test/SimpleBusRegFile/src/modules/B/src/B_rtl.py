a9e061574a2c03ffcbc170e7eee591b15cbcec9e
Hristo Nikolov <h.n.nikolov@gmail.com>
2017-05-31 22:39:24 +0200

    Renamed signals to 'fifo' and 'my_b_reg'.

--- test/SimpleBusRegFile/src/modules/B/src/B_rtl.py
+++ test/SimpleBusRegFile/src/modules/B/src/B_rtl.py
@@ -27,34 +27,34 @@ def B_rtl(rst, clk, sbus):
     # Local usage of ctrl
     re, rdata, we, wdata = sbus.ctrl.get_interface('rw', 4*8, 'reg')
 
-    c = Signal(modbv(0)[4*8:])
+    my_b_reg = Signal(modbv(0)[4*8:])
 
     @always_seq(clk.posedge, reset=rst)
-    def my_c():
+    def prcs_my_b_reg():
         if we:
-            c.next = wdata
+            my_b_reg.next = wdata
 
     @always_comb
     def comb():
-        rdata.next = c if re == 1 else 0
+        rdata.next = my_b_reg if re == 1 else 0
 
     we_1, wdata_1 = sbus.ctrl.get_interface('wo', 4*8, 'w_reg')
     re_1, rdata_1 = sbus.ctrl.get_interface('ro', 4*8, 'r_reg')
 
     # Connect a FIFO to the SimpleBus --------------------------
-    wreg = HSD(data=4*8, buf_size=4)
+    fifo = HSD(data=4*8, buf_size=4)
     
-    wreg_w_rdy, wreg_w_vld, wreg_w_data = wreg.get_src_signals() 
-    wreg_r_rdy, wreg_r_vld, wreg_r_data = wreg.get_snk_signals() 
+    fifo_w_rdy, fifo_w_vld, fifo_w_data = fifo.get_src_signals()
+    fifo_r_rdy, fifo_r_vld, fifo_r_data = fifo.get_snk_signals()
     
     # Writes and reads are not blocking.
     # To get the FIFO status, we need to map emty and full to another address/register
     @always_comb
     def assign_data():
-        wreg_w_vld.next  = we_1
-        wreg_w_data.next = wdata_1
-        wreg_r_rdy.next  = re_1
-        rdata_1.next     = wreg_r_data
+        fifo_w_vld.next  = we_1
+        fifo_w_data.next = wdata_1
+        fifo_r_rdy.next  = re_1
+        rdata_1.next     = fifo_r_data
 
     #--- Custom code end   ---#
 
